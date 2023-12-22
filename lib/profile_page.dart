7af03ec713685fcc3db579b72faf9f1ba03b2644
RobinSardja <robinsardja@gmail.com>
2023-12-21 19:59:00 -0500

    Saves user data to firestore.

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 // import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
@@ -15,13 +16,36 @@ class _ProfilePageState extends State<ProfilePage> {
 
   // static const googleClientID = String.fromEnvironment("google", defaultValue: "none");
   bool loggedIn = false;
+  final userDetection = FirebaseAuth.instance.authStateChanges();
+  late User currentUser;
+  late Map<String, dynamic> userData;
+  late FirebaseFirestore db;
 
   @override
   Widget build( BuildContext context ) {
     return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
+      stream: userDetection,
       builder: (context, snapshot) {
+
         loggedIn = snapshot.hasData;
+
+        // get current user
+        userDetection.listen((User? user) {
+          if( user != null ) {
+            currentUser = user;
+          }
+        });
+
+        db = FirebaseFirestore.instance;
+
+        // get current user's data
+        final docRef = db.collection( "user_options" ).doc( currentUser.uid );
+        docRef.get().then(
+          (DocumentSnapshot doc) {
+            userData = doc.data() as Map<String, dynamic>;
+          }
+        );
+
         return loggedIn ?
         ProfileScreen( // profile screen to show when user already logged in
           actions: [
@@ -60,17 +84,66 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  late String snackBarMessage;
 
   Widget editProfilePage() {
+
+    late String snackBarMessage;
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: const Center( child: Text( "Edit profile" ) ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text( "edit profile page" )
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Center(
+              child: DropdownMenu( // selected blood type
+                label: const Text( "Select blood type" ),
+                initialSelection: userData["bloodType"],
+                onSelected: (selectedEntry) {
+                  userData["bloodType"] = selectedEntry;
+                },
+                dropdownMenuEntries: const [
+                  DropdownMenuEntry(
+                    value: "O+",
+                    label: "O+",
+                  ),
+                  DropdownMenuEntry(
+                    value: "O-",
+                    label: "O-",
+                  ),
+                  DropdownMenuEntry(
+                    value: "A+",
+                    label: "A+",
+                  ),
+                  DropdownMenuEntry(
+                    value: "A-",
+                    label: "A-",
+                  ),
+                  DropdownMenuEntry(
+                    value: "B+",
+                    label: "B+",
+                  ),
+                  DropdownMenuEntry(
+                    value: "B-",
+                    label: "B-",
+                  ),
+                  DropdownMenuEntry(
+                    value: "AB+",
+                    label: "AB+",
+                  ),
+                  DropdownMenuEntry(
+                    value: "AB-",
+                    label: "AB-",
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -85,6 +158,14 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
         onTap: (selectedIndex) {
           snackBarMessage = selectedIndex == 0 ? "Profile changes saved!" : "Profile changes deleted";
+
+          if( selectedIndex == 0 ) {
+            db
+              .collection( "user_options" )
+              .doc( currentUser.uid )
+              .set( userData );
+          }
+
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: Text(snackBarMessage),
