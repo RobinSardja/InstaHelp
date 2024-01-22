bd5dc95bdfc303d501fc54cf34f784e00a747bcf
RobinSardja <robinsardja@gmail.com>
2024-01-22 14:31:43 -0500

    Edit profile page discards changes on back button.

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _InstaHelpState extends State<InstaHelp> {
 
     for( int i = 0; i < userData["emergencyContacts"].length; i++ ) { // extracts phone number
       userData["emergencyContacts"][i] = String.fromCharCodes( userData["emergencyContacts"][i].toString().codeUnits.where( (x) => (x ^0x30) <= 9 ) );
-    } // TODO: text message not sending
+    } // TODO: text message not sending. phone number extraction works just fine though
 
     await getPosition();
     try {
@@ -84,7 +84,7 @@ class _InstaHelpState extends State<InstaHelp> {
             "www.google.com/maps/search/${currentPosition.latitude},${currentPosition.longitude}"
             "/@${currentPosition.latitude},${currentPosition.longitude}! "
             "${userData["medicalInfo"] ? "Blood Type: ${userData["bloodType"]}" : "" }",
-          recipients: userData["emergencyContacts"],
+          recipients: userData["emergencyContacts"] ??= defaultData["emergencyContacts"],
           sendDirect: true,
         );
     } on Error {
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -219,251 +219,254 @@ class EditProfilePageState extends State<EditProfilePage> {
   @override
   Widget build( BuildContext context ) {
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text( "Edit profile" ),
-      ),
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            ListTile( // choose to send medical information
-              title: const Text( "Send medical information" ),
-              trailing: Switch(
-                value: medicalInfo,
-                onChanged: (value) {
-                  setState( () => medicalInfo = value );
-                },
-              ),
-            ),
-            Center(
-              child: DropdownMenu( // selected blood type
-                enabled: medicalInfo,
-                label: Text( medicalInfo ? "Select blood type" : "Disabled" ),
-                initialSelection: medicalInfo ? bloodType : "Disabled",
-                onSelected: (value) => bloodType = value as String,
-                dropdownMenuEntries: const [
-                  DropdownMenuEntry(
-                    value: "O+",
-                    label: "O+",
-                  ),
-                  DropdownMenuEntry(
-                    value: "O-",
-                    label: "O-",
-                  ),
-                  DropdownMenuEntry(
-                    value: "A+",
-                    label: "A+",
-                  ),
-                  DropdownMenuEntry(
-                    value: "A-",
-                    label: "A-",
-                  ),
-                  DropdownMenuEntry(
-                    value: "B+",
-                    label: "B+",
-                  ),
-                  DropdownMenuEntry(
-                    value: "B-",
-                    label: "B-",
-                  ),
-                  DropdownMenuEntry(
-                    value: "AB+",
-                    label: "AB+",
-                  ),
-                  DropdownMenuEntry(
-                    value: "AB-",
-                    label: "AB-",
-                  ),
-                  DropdownMenuEntry(
-                    value: "Disabled",
-                    label: "Disabled",
-                    enabled: false,
-                  ),
-                ],
-              ),
-            ),
-            ListTile( // alert nearby users
-              title: const Text( "Alert nearby users (in future!)" ),
-              trailing: Switch(
-                value: alertNearbyUsers,
-                onChanged: (value) {
-                  setState(() {
-                    alertNearbyUsers = value;
-                    if( value == false ) {
-                      proximityDistance = 5.0;
-                    }
-                  });
-                },
-              ),
-            ),
-            Center(
-              child: Text(
-                alertNearbyUsers ? "Proximity distance: $proximityDistance ${proximityDistance == 1 ? "mile" : "miles"}" : "Location signal disabled"
-              ),
-            ),
-            Center(
-              child: Slider( // proximity distance in miles
-                min: 1.0,
-                max: 10.0,
-                divisions: 18,
-                value: alertNearbyUsers ? proximityDistance : 1.0,
-                onChanged: (value) {
-                  if( alertNearbyUsers ) {
-                    setState( () => proximityDistance = value );
-                  }
-                },
-                thumbColor: alertNearbyUsers ? Colors.red : Colors.black,
-              ),
-            ),
-            ListTile( // send text message to designated emergency contacts
-              title: const Text( "Text message alert" ),
-              trailing: Switch(
-                value: textMessageAlert,
-                onChanged: (value) {
-                  setState(() {
-                    textMessageAlert = value;
-                    if( value == false ) {
-                      emergencyContacts = [];
-                    }
-                  });
-                },
-              ),
-            ),
-            Center( // button to add emergency contacts
-              child: ElevatedButton(
-                onPressed: () async {
-                  if( textMessageAlert && emergencyContacts.length < _maxEmergencyContacts ) {
-                    Contact? value = await _contactPicker.selectContact();                                                            
-                    if( value != null && !emergencyContacts.contains(value.toString()) ) {
-                      setState( () => emergencyContacts.add( value.toString() ) );
-                    }
-                  }
-                },
-                child: Text(
-                  textMessageAlert ? emergencyContacts.length == _maxEmergencyContacts ? "Maximum $_maxEmergencyContacts emergency contacts" : "Add new emergency contact" : "Text message alert disabled"
+    return PopScope(
+      onPopInvoked: (didPop) => updateFirestore(),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text( "Edit profile" ),
+        ),
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              ListTile( // choose to send medical information
+                title: const Text( "Send medical information" ),
+                trailing: Switch(
+                  value: medicalInfo,
+                  onChanged: (value) {
+                    setState( () => medicalInfo = value );
+                  },
                 ),
               ),
-            ),
-            ListView.builder( // TODO: not discarding on back button
-              shrinkWrap: true,
-              physics: const ClampingScrollPhysics(),
-              itemCount: emergencyContacts.length,
-              itemBuilder: (_, int index) {
-                return ListTile(
-                  title: Text( emergencyContacts[index] ),
-                  trailing: IconButton(
-                    icon: const Icon( Icons.delete ),
-                    onPressed: () {
-                      setState( () => emergencyContacts.remove( emergencyContacts[index] ) );
-                    },
-                  ),
-                );
-              },
-            ),
-            ListTile( // play loud siren
-              title: const Text( "Sound alarm" ),
-              trailing: Switch(
-                value: soundAlarm,
-                onChanged: (value) {
-                  setState(() {
-                    soundAlarm = value;
-                    _volumeController.getVolume().then( (volume) => _setVolumeValue = volume );
-                  });
-                },
+              Center(
+                child: DropdownMenu( // selected blood type
+                  enabled: medicalInfo,
+                  label: Text( medicalInfo ? "Select blood type" : "Disabled" ),
+                  initialSelection: medicalInfo ? bloodType : "Disabled",
+                  onSelected: (value) => bloodType = value as String,
+                  dropdownMenuEntries: const [
+                    DropdownMenuEntry(
+                      value: "O+",
+                      label: "O+",
+                    ),
+                    DropdownMenuEntry(
+                      value: "O-",
+                      label: "O-",
+                    ),
+                    DropdownMenuEntry(
+                      value: "A+",
+                      label: "A+",
+                    ),
+                    DropdownMenuEntry(
+                      value: "A-",
+                      label: "A-",
+                    ),
+                    DropdownMenuEntry(
+                      value: "B+",
+                      label: "B+",
+                    ),
+                    DropdownMenuEntry(
+                      value: "B-",
+                      label: "B-",
+                    ),
+                    DropdownMenuEntry(
+                      value: "AB+",
+                      label: "AB+",
+                    ),
+                    DropdownMenuEntry(
+                      value: "AB-",
+                      label: "AB-",
+                    ),
+                    DropdownMenuEntry(
+                      value: "Disabled",
+                      label: "Disabled",
+                      enabled: false,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Center(
-              child: Text(
-                soundAlarm ? "Phone volume: ${_volumeListenerValue == 0 ? "Muted" : "${(_volumeListenerValue * 100).round()}%" }" : "Sound alarm disabled"
-              ),
-            ),
-            Center(
-              child: Slider( // siren volume
-                min: 0,
-                max: 1,
-                value: soundAlarm ? _volumeListenerValue : 0,
-                onChanged: (value) {
-                  if( soundAlarm ) {
+              ListTile( // alert nearby users
+                title: const Text( "Alert nearby users (in future!)" ),
+                trailing: Switch(
+                  value: alertNearbyUsers,
+                  onChanged: (value) {
                     setState(() {
-                      _setVolumeValue = value;
-                      _volumeController.setVolume(_setVolumeValue);
+                      alertNearbyUsers = value;
+                      if( value == false ) {
+                        proximityDistance = 5.0;
+                      }
                     });
-                  }
+                  },
+                ),
+              ),
+              Center(
+                child: Text(
+                  alertNearbyUsers ? "Proximity distance: $proximityDistance ${proximityDistance == 1 ? "mile" : "miles"}" : "Location signal disabled"
+                ),
+              ),
+              Center(
+                child: Slider( // proximity distance in miles
+                  min: 1.0,
+                  max: 10.0,
+                  divisions: 18,
+                  value: alertNearbyUsers ? proximityDistance : 1.0,
+                  onChanged: (value) {
+                    if( alertNearbyUsers ) {
+                      setState( () => proximityDistance = value );
+                    }
+                  },
+                  thumbColor: alertNearbyUsers ? Colors.red : Colors.black,
+                ),
+              ),
+              ListTile( // send text message to designated emergency contacts
+                title: const Text( "Text message alert" ),
+                trailing: Switch(
+                  value: textMessageAlert,
+                  onChanged: (value) {
+                    setState(() {
+                      textMessageAlert = value;
+                      if( value == false ) {
+                        emergencyContacts = [];
+                      }
+                    });
+                  },
+                ),
+              ),
+              Center( // button to add emergency contacts
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if( textMessageAlert && emergencyContacts.length < _maxEmergencyContacts ) {
+                      Contact? value = await _contactPicker.selectContact();                                                            
+                      if( value != null && !emergencyContacts.contains(value.toString()) ) {
+                        setState( () => emergencyContacts.add( value.toString() ) );
+                      }
+                    }
+                  },
+                  child: Text(
+                    textMessageAlert ? emergencyContacts.length == _maxEmergencyContacts ? "Maximum $_maxEmergencyContacts emergency contacts" : "Add new emergency contact" : "Text message alert disabled"
+                  ),
+                ),
+              ),
+              ListView.builder( // TODO: not discarding on back button
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemCount: emergencyContacts.length,
+                itemBuilder: (_, int index) {
+                  return ListTile(
+                    title: Text( emergencyContacts[index] ),
+                    trailing: IconButton(
+                      icon: const Icon( Icons.delete ),
+                      onPressed: () {
+                        setState( () => emergencyContacts.remove( emergencyContacts[index] ) );
+                      },
+                    ),
+                  );
                 },
-                thumbColor: soundAlarm ? Colors.red : Colors.black,
               ),
+              ListTile( // play loud siren
+                title: const Text( "Sound alarm" ),
+                trailing: Switch(
+                  value: soundAlarm,
+                  onChanged: (value) {
+                    setState(() {
+                      soundAlarm = value;
+                      _volumeController.getVolume().then( (volume) => _setVolumeValue = volume );
+                    });
+                  },
+                ),
+              ),
+              Center(
+                child: Text(
+                  soundAlarm ? "Phone volume: ${_volumeListenerValue == 0 ? "Muted" : "${(_volumeListenerValue * 100).round()}%" }" : "Sound alarm disabled"
+                ),
+              ),
+              Center(
+                child: Slider( // siren volume
+                  min: 0,
+                  max: 1,
+                  value: soundAlarm ? _volumeListenerValue : 0,
+                  onChanged: (value) {
+                    if( soundAlarm ) {
+                      setState(() {
+                        _setVolumeValue = value;
+                        _volumeController.setVolume(_setVolumeValue);
+                      });
+                    }
+                  },
+                  thumbColor: soundAlarm ? Colors.red : Colors.black,
+                ),
+              ),
+              ListTile( // turn flashlight on and off like a blinker
+                title: const Text( "Blink flashlight" ),
+                trailing: Switch(
+                  value: blinkFlashlight,
+                  onChanged: (value) {
+                    setState( () => blinkFlashlight = value );
+                    if( value == false ) {
+                      blinkSpeed = 250;
+                    }
+                  },
+                ),
+              ),
+              Center(
+                child: Text(
+                  blinkFlashlight ? "Blink speed: ${ blinkSpeed == 1000 ? "1 second" : "${blinkSpeed.round()} milliseconds" }" : "Blink flashlight disabled"
+                ),
+              ),
+              Center(
+                child: Slider( // proximity distance in miles
+                  min: 100,
+                  max: 1000,
+                  divisions: 18,
+                  value: blinkFlashlight ? blinkSpeed : 100.0,
+                  onChanged: (value) {
+                    if( blinkFlashlight ) {
+                      setState(() => blinkSpeed = value );
+                    }
+                  },
+                  thumbColor: blinkFlashlight ? Colors.red : Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (updatedIndex) {
+      
+            if( updatedIndex == 0 ) {
+              _setFinalVariables();
+              db
+                .collection( "user_options" )
+                .doc( currentUser.uid )
+                .set( userData );
+            }
+      
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text( updatedIndex == 0 ? "Profile changes saved!" : "Profile changes discarded" ),
+                behavior: SnackBarBehavior.floating,
+                action: SnackBarAction(
+                  label: "OK",
+                  onPressed: () {},
+                ),
+              ),
+            );
+      
+            updateFirestore();
+            Navigator.pop(context);
+          },
+          destinations: const [
+            NavigationDestination(
+              icon: Icon( Icons.save ),
+              label: "Save",
             ),
-            ListTile( // turn flashlight on and off like a blinker
-              title: const Text( "Blink flashlight" ),
-              trailing: Switch(
-                value: blinkFlashlight,
-                onChanged: (value) {
-                  setState( () => blinkFlashlight = value );
-                  if( value == false ) {
-                    blinkSpeed = 250;
-                  }
-                },
-              ),
-            ),
-            Center(
-              child: Text(
-                blinkFlashlight ? "Blink speed: ${ blinkSpeed == 1000 ? "1 second" : "${blinkSpeed.round()} milliseconds" }" : "Blink flashlight disabled"
-              ),
-            ),
-            Center(
-              child: Slider( // proximity distance in miles
-                min: 100,
-                max: 1000,
-                divisions: 18,
-                value: blinkFlashlight ? blinkSpeed : 100.0,
-                onChanged: (value) {
-                  if( blinkFlashlight ) {
-                    setState(() => blinkSpeed = value );
-                  }
-                },
-                thumbColor: blinkFlashlight ? Colors.red : Colors.black,
-              ),
+            NavigationDestination(
+              icon: Icon( Icons.delete ),
+              label: "Discard",
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (updatedIndex) {
-
-          if( updatedIndex == 0 ) {
-            _setFinalVariables();
-            db
-              .collection( "user_options" )
-              .doc( currentUser.uid )
-              .set( userData );
-          }
-
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text( updatedIndex == 0 ? "Profile changes saved!" : "Profile changes discarded" ),
-              behavior: SnackBarBehavior.floating,
-              action: SnackBarAction(
-                label: "OK",
-                onPressed: () {},
-              ),
-            ),
-          );
-
-          updateFirestore();
-          Navigator.pop(context);
-        },
-        destinations: const [
-          NavigationDestination(
-            icon: Icon( Icons.save ),
-            label: "Save",
-          ),
-          NavigationDestination(
-            icon: Icon( Icons.delete ),
-            label: "Discard",
-          ),
-        ],
       ),
     );
   }
